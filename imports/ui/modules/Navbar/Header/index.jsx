a15d40b29bebb06db83a25c3257268c364533441
RomainR <rousseau.romain82@gmail.com>
2019-06-11 22:25:54 +0200

    Test send function with  props

--- imports/ui/modules/Navbar/Header/index.jsx
+++ imports/ui/modules/Navbar/Header/index.jsx
@@ -8,70 +8,71 @@ import { fade, makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
-import Sidenav from './SideNav'
 
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
+
+
+const Header = (props) => {
+
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
     },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
+    menuButton: {
+      marginRight: theme.spacing(2),
     },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    width: theme.spacing(7),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 7),
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: 120,
-      '&:focus': {
-        width: 200,
+    title: {
+      flexGrow: 1,
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
       },
     },
-  },
-}));
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 7),
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: 120,
+        '&:focus': {
+          width: 200,
+        },
+      },
+    },
+  }));
 
-export default function Header() {
   const classes = useStyles();
   test = () => {
     console.log('zeezf')
   }
   return (
     <div className={classes.root}>
-      <Sidenav ></Sidenav>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -79,6 +80,7 @@ export default function Header() {
             className={classes.menuButton}
             color="inherit"
             aria-label="Open drawer"
+            onClick={props.plus}
             onClick={test}
           >
             <MenuIcon />
@@ -104,3 +106,4 @@ export default function Header() {
     </div>
   );
 }
+export default Header
--- imports/ui/modules/Navbar/index.jsx
+++ imports/ui/modules/Navbar/index.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import Header from './Header'
+import Sidenav from './SideNav'
 
 const NavBar = () => {
+  let [ count, setCount ] = useState(0);
 
+  const plus = useCallback(() => {
+    setCount(count+1)
+    console.log(count)
+  }, [setCount])
     return (
       <div>
-          <Header></Header>
+        <h3 >{count}</h3>
+        <Sidenav></Sidenav>
+        <Header plus={plus}></Header>
       </div>
     );
   }
