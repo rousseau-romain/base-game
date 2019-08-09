5f80a7f2e675c6defe7b8b8fae3ce3cd4e27f7d3
RomainR <rousseau.romain82@gmail.com>
2019-08-09 22:31:22 +0200

    Remove unused last name

--- imports/ui/modules/Accounts/Signup/index.jsx
+++ imports/ui/modules/Accounts/Signup/index.jsx
@@ -70,7 +70,7 @@ const Signup = () => {
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}>
               <TextField
                 value={username}
                 onChange={(e) => { setUsername(e.target.value); }}
@@ -80,19 +80,8 @@ const Signup = () => {
                 required
                 fullWidth
                 id="firstName"
-                label="First Name"
+                label="Username"
                 autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
               />
             </Grid>
             <Grid item xs={12}>
