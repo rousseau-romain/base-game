63600ac3003a3a1a4ebc7d82bef9a6f98c84a8f8
RomainR <rousseau.romain82@gmail.com>
2019-08-11 22:45:58 +0200

    Add button add NewGame

---
+++ imports/ui/modules/Games/AddButton.jsx
@@ -0,0 +1,13 @@
+import Fab from '@material-ui/core/Fab';
+import styled from 'styled-components';
+
+
+const AddButon = styled(Fab)`
+  &&&{
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+  }
+`;
+
+export default AddButon;
--- imports/ui/modules/Games/index.jsx
+++ imports/ui/modules/Games/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '/imports/ui/components/Navbar';
+import AddIcon from '@material-ui/icons/Add';
+import AddButton from './AddButton';
+
 
 const Games = () => (
   <div>
@@ -8,6 +11,7 @@ const Games = () => (
     <h3>Games</h3>
     <Link to="/signup">Inscription</Link>
     <Link to="/signin">Connection</Link>
+    <AddButton><AddIcon /></AddButton>
   </div>
 );
 
