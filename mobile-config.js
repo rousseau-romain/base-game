4ff10de7f6cc2f22e8840d98ae47f27ef2c6ba8c
Romain Rousseau <romain.r@bricoprive.com>
2019-11-20 10:39:06 +0100

    Add mobile icons

---
+++ mobile-config.js
@@ -0,0 +1,7 @@
+App.icons({
+  android_mdpi: './imports/img/ic_launcher-48.png',
+  android_hdpi: './imports/img/ic_launcher-72.png',
+  android_xhdpi: './imports/img/ic_launcher-96.png',
+  android_xxhdpi: './imports/img/ic_launcher-144.png',
+  android_xxxhdpi: './imports/img/ic_launcher-192.png',
+});
