ecb9b1f06a4205149511fe634c599423bfba1071
RomainR <rousseau.romain82@gmail.com>
2019-12-10 22:36:01 +0100

    Fix change ulr user/game

--- imports/ui/modules/User/Games/index.jsx
+++ imports/ui/modules/User/Games/index.jsx
@@ -24,7 +24,7 @@ const Games = ({ history }) => {
     });
   }, []);
 
-  const goNewGame = useCallback(() => { history.push('/game/new'); }, [history]);
+  const goNewGame = useCallback(() => { history.push('/user/game/new'); }, [history]);
 
   const deleteGame = useCallback((id, e) => {
     e.stopPropagation();
