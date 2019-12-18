9d4a140492ca86aee1730a4dc56b494ce9057653
Romain Rousseau <romain.r@bricoprive.com>
2019-12-18 23:50:28 +0100

    change icon app add launch screen

--- mobile-config.js
+++ mobile-config.js
@@ -5,6 +5,13 @@ App.icons({
   android_xxhdpi: './imports/img/ic_launcher-144.png',
   android_xxxhdpi: './imports/img/ic_launcher-192.png',
 });
+App.launchScreens({
+  android_mdpi_portrait: './imports/img/android-320-480.png',
+  android_hdpi_portrait: './imports/img/android-480-800.png',
+  android_xhdpi_portrait: './imports/img/android-720-1280.png',
+  android_xxhdpi_portrait: './imports/img/android-960-1600.png',
+  android_xxxhdpi_portrait: './imports/img/android-1280-1920.png',
+})
 App.accessRule('*');
 App.setPreference('StatusBarBackgroundColor', '#3f51b5');
 App.setPreference('StatusBarStyle', 'lightcontent');
